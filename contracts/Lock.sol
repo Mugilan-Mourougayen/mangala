3b7156a2bda82ec62b8557721d67ea008f8e5d7f
Mugilan-Mourougayen <mugilanmourougayen@gmail.com>
2022-11-18 15:04:28 +0100

    first commit

---
+++ contracts/Lock.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Lock {
+  
+     Twitter[] public tweet;
+     uint256 public counter;
+     string public value;
+     string public valuemsg;
+     bytes32 public value1;
+     bytes32 public value2;
+    struct Twitter {
+         string _tweetmsg;
+         string _nickName;
+         address _address;
+     }
+     
+     function addTwitter(string memory _tweetmsg,string memory _nickName) public {
+        tweet.push(Twitter(_tweetmsg,_nickName, msg.sender));
+        counter +=1;
+        
+     }
+    //  function tweetcontent() public view returns(string memory){
+    //     return tweet;
+    //  }
+
+       function replace(string memory _msg,string memory _newmesg) public {
+      
+        for(uint i=0; i<counter;i++){
+          if(keccak256(bytes(tweet[i]._tweetmsg)) == keccak256(bytes(_msg))  && tweet[i]._address == msg.sender ){
+            tweet[i]._tweetmsg = _newmesg;
+          }
+          else{
+            value ="notfound";
+          }
+        }
+        
+     }
+
+ 
+           
+           
+           
+          
+            
+
+}
