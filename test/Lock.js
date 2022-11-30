c40274acf0b58a6b7529165f87004bcb3cfbbe19
Mugilan-Mourougayen <mugilanmourougayen@gmail.com>
2022-11-30 12:55:14 +0100

    Tweet Ni

--- test/Lock.js
+++ test/Lock.js
@@ -10,7 +10,7 @@ const { ethers } = require("hardhat");
   describe("Deployment", function () {
    
 
-    it("Should set the right owner", async function () {
+    it("App has been deployed and address has to matched", async function () {
       const [owner, otherAccount] = await ethers.getSigners();
 
       const Lock = await ethers.getContractFactory("Lock");
@@ -23,36 +23,161 @@ assert.equal(await lock.signer.address, owner.address);
 
 
 
-    describe("Add Tweet", function () {
+  describe("Add Tweet", function () {
    
 
-      it("Should set the right owner", async function () {
-        const [owner, otherAccount] = await ethers.getSigners();
+    it("user able to add tweet", async function () {
+      const [owner, otherAccount] = await ethers.getSigners();
 
-        const Lock = await ethers.getContractFactory("Lock");
-        const lock = await Lock.deploy();
-   
-  
-        const currentcount = await lock.getResult()
+      const Lock = await ethers.getContractFactory("Lock");
+      const lock = await Lock.deploy();
+ 
 
-
-     const currentlength = currentcount.length
-
-     const add = await lock.addTwitter("hello","mugilan")
-     const newcount = await lock.getResult()
+      const currentcount = await lock.getResult()
 
 
-     const newlength = newcount.length
-     console.log(currentlength,newlength)
-    //  assert.equal(currentlength,(newlength+1));
-    expect(currentlength).to.equal((newlength+1));
-  // assert.equal(await lock.signer.address, owner.address);
-  
-      });
-  
+   const currentlength = currentcount.length
 
-      
+   const add = await lock.addTwitter("hello","mugilan")
+   const newcount = await lock.getResult()
+
+
+   const newlength = newcount.length
+
+   expect(currentlength).to.equal(0); 
+  expect(newlength).to.equal(1); 
+
+    });
+
+    
+
+
+    
 
 
 
 });
+
+
+
+
+describe("Delete Tweet", function () {
+   
+
+  it("user able to delete tweet", async function () {
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Lock = await ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy();
+
+
+ const add = await lock.addTwitter("hello","mugilan")
+ const currentcount = await lock.getResult()
+ const currentlength = currentcount.length
+
+
+ const remove = await lock.remove(0)
+ const newcount = await lock.getResult()
+ const newlength = newcount.length
+
+
+ expect(currentlength).to.equal(1); 
+ expect(newlength).to.equal(0); 
+
+
+  });
+
+});
+
+
+
+
+
+
+
+
+
+describe("Update Tweet", function () {
+   
+
+  it("user able to update tweet", async function () {
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Lock = await ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy();
+
+
+ const add = await lock.addTwitter("hello","mugilan")
+ const currentdetail = await lock.getResult()
+
+
+ const edit = await lock.replace("hello","hai",currentdetail[0][3])
+ const newdetail = await lock.getResult()
+
+
+//  const remove = await lock.remove(0)
+//  const newcount = await lock.getResult()
+//  const newlength = newcount.length
+
+ expect(newdetail[0][0]).to.not.equal(currentdetail[0][0]);
+
+
+  });
+
+});
+
+
+describe("Read Tweet", function () {
+   
+
+  it("user able to read tweet", async function () {
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Lock = await ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy();
+
+
+ const add1 = await lock.addTwitter("hai","mugilan")
+ const add2 = await lock.addTwitter("hello","mugilan")
+ const add3 = await lock.addTwitter("bye","mugilan")
+ const currentdetail = await lock.getResult()
+ const length = currentdetail.length
+ expect(length).to.equal(3); 
+
+
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
